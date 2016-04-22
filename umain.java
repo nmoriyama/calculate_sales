c27284d6d4cdfaa9e2eea106dd2cfdd70c1f8dd7
moriyama <m.nao1026@gmail.com>
2016-04-22 11:53:48 +0900

    ３．集計を追加

--- umain.java
+++ umain.java
@@ -1,3 +1,5 @@
+//package jp.co.plusize.moriyama_naoki.calculate_sales;
+
 import java.io.*;
 import java.util.*;
 
@@ -6,9 +8,12 @@ public class umain {
 		ArrayList<Integer> dataList = new ArrayList<Integer>();
 		ArrayList<Integer> branchList1 = new ArrayList<Integer>();
 		ArrayList branchList2 = new ArrayList();
-		ArrayList<Integer> commodityList1 = new ArraryList<Integer>();
-		ArrayList commodityList2 = new ArraryList();
-		int x = 0;
+		ArrayList<Integer> commodityList1 = new ArrayList<Integer>();
+		ArrayList commodityList2 = new ArrayList();
+		HashMap<String,Integer> map1=new HashMap<String,Integer>();
+		HashMap<String,Integer> map2=new HashMap<String,Integer>();
+		int x = 0,sumdata1 = 0,sumdata2 = 0,a = 0,b = 1,c = 2;
+		
 		try{                                     //1-1支店定義ファイルの読み込み
 			File file1 = new File(args[0]+"\\branch.lst");
 			FileReader fr1 = new FileReader(file1);
@@ -21,6 +26,7 @@ public class umain {
 				if(x%2 == 0){
 					int data1 = Integer.parseInt(branch[x]);
 					branchList1.add(data1);
+					map1.put(data1,0);
 				}else{
 					branchList2.add(branch[x]);
 				}
@@ -40,10 +46,11 @@ public class umain {
 				String[] commodity = s2.split(",");//奇数:支店コード、偶数:支店名
 				x++;
 				if(x%2 == 0){
-					int data2 = Integer.parseInt(comodity[x]);
+					int data2 = Integer.parseInt(commodity[x]);
 					commodityList1.add(data2);
+					map2.put(data2,0);
 				}else{
-					commodityList2.add(comodity[x]);
+					commodityList2.add(commodity[x]);
 				}
 			}
 			br2.close();
@@ -51,38 +58,65 @@ public class umain {
 			System.out.println("商品定義ファイルのフォーマットが不正です");
 		}
 		
-		try{              //1-3
-			for(int data = 0;data<100;data++){
+		try{              //3-1
+			for(int data = 0;data<100;data++){//仮
 				//このままだと８桁の数字ではないので文字列にする必要
 				File file3 = new File(args[0]+"\\"+data+".rcd");
+				if(!file3.exists()){
+					int plot = data;
+					plot++;
+					File file4 = new File(args[0]+"\\"+plot+".rcd");
+					if(file4.exists()){
+						throw new IOException("連番になっていません");
+					}
+				}
 				FileReader fr3 = new FileReader(file3);
 				BufferedReader br3 = new BufferedReader(fr3);
 				String s3;
-				while((s3 = br3.readLine())! = null){
-					dataList.add(s3);
+				while((s3 = br3.readLine()) != null){
+					int s = Integer.parseInt(s3);
+					dataList.add(s);
+				}
+				                              //3-2 加算していく
+
+				try{                        //支店コードが見つからなかったとき
+					if(map1.get(dataList.get(0)) == null){
+						throw new IOException("支店コードが不正です");
+					}else{
+						sumdata1=map1.get(dataList.get(a))+dataList.get(c);
+					}
+				}catch(IOException e){
+					System.out.println(dataList.get(a)+"の支店コードが不正です");
+				}
+
+				try{                        //商品コードが見つからなかったとき
+					if(map1.get(dataList.get(b)) == null){
+						throw new IOException("商品コードが不正です");
+					}else{
+						sumdata2=map2.get(dataList.get(b))+dataList.get(c);
+					}
+				}catch(IOException e){
+					System.out.println(dataList.get(b)+"の商品コードが不正です");
+				}
+
+				try{                        //４行以上あるとき
+				}catch(IOException e){
+					System.out.println(data+".rcdのフォーマットが不正です");
 				}
 				
-				try{                               //加算していく
-					File file3 = new File(args[0]+"\\"+data+".rcd");
-					
-					try{                        //支店コードが見つからなかったとき
-						
-					}catch(IOException e){
-						System.out.println(+"の支店コードが不正です");
+				try{
+					if(sumdata1 > 10*10){
+						throw new IOException("合計金額が10桁を超えました");
 					}
-
-					try{                        //商品コードが見つからなかったとき
-						
-					}catch(IOException e){
-						System.out.println(+"の商品コードが不正です");
-					}
-					try{                        //４行以上あるとき
-					}catch(IOException e){
-						System.out.println(+"のフォーマットが不正です");
+					if(sumdata2 > 10*10){
+						throw new IOException("合計金額が10桁を超えました");
 					}
 				}catch(IOException e){
 					System.out.println("合計金額が10桁を超えました");
 				}
+				a += 1;
+				b += 1;
+				c += 1;
 			}
 		}catch(IOException e){
 			System.out.println("売上ファイル名が連番になっていません");

b424ceaac51a8e1e780a17d147f3483c78e6c304
moriyama <m.nao1026@gmail.com>
2016-04-26 09:48:33 +0900

    4/26　完成

--- calculate_sales.java
+++ calculate_sales.java
@@ -29,30 +29,36 @@ public class calculate_sales {
 		
 		try{//1支店定義ファイルの読み込み
 			File file1 = new File(args[0] + "\\branch.lst");
+			if(!file1.exists()){
+				throw new Exception("支店定義ファイルが存在しません");
+			}
 			FileReader fr1 = new FileReader(file1);
 			BufferedReader br1 = new BufferedReader(fr1);
 
 			String s1;
-	
+			
 			while((s1 = br1.readLine()) != null){
+				System.out.println(s1);
 				String[] branch = s1.split(",");// , で分割
 				branchmap1.put(branch[0],0);//キー:支店コード , 要素:売上金額
 				branchmap2.put(branch[0],branch[1]);//キー:支店コード , 要素:支店名
 				//１行 , が２つ以上多くある もしくは 支店コードが３桁でない 場合
 				if(branch.length > 2 || branch[0].length() != 3){ 
-					throw new 	ArrayStoreException ("支店定義ファイルのフォーマットが不正です");
+					br1.close();
+					throw new 	Exception("支店定義ファイルのフォーマットが不正です");
 				}
 			}
 			
 			br1.close();
-		}catch(	ArrayStoreException  e){
-			System.out.println("支店定義ファイルのフォーマットが不正です");
-		}catch(IOException e){
-			System.out.println("支店定義ファイルが存在しません");
+		}catch(	Exception  e){
+			System.out.println(e);
 		}
 		
 		try{ //2商品定義ファイルの読み込み
 			File file2 = new File(args[0] + "\\commodity.lst");
+			if(!file2.exists()){
+				throw new Exception("商品定義ファイルが存在しません");
+			}
 			FileReader fr2 = new FileReader(file2);
 			BufferedReader br2 = new BufferedReader(fr2);
 			String s2;
@@ -63,15 +69,15 @@ public class calculate_sales {
 				commap2.put(commodity[0],commodity[1]);//キー:商品コード , 要素:商品名
 				//１行 , が２つ以上多くある もしくは 商品コードが８桁でない 場合
 				if(commodity.length > 2 || commodity[0].length() != 8){
-					throw new 	ArrayStoreException ("商品定義ファイルのフォーマットが不正です");
+					br2.close();
+					throw new IllegalArgumentException ("商品定義ファイルのフォーマットが不正です");
 				}
 			}
 			br2.close();
-		}catch(	ArrayStoreException  e){
-			System.out.println("商品定義ファイルのフォーマットが不正です");
-		}catch(IOException e){
-			System.out.println("商品定義ファイルが存在しません");
+		}catch(Exception  e){
+			System.out.println(e);
 		}
+
 		//3集計
 		//売上ファイルのある場所の検索
 		File file3 = new File(args[0]);
@@ -108,9 +114,9 @@ public class calculate_sales {
 					s2 = s2.concat(X);
 					file4 = new File(args[0] + "\\" + s2 + ".rcd");
 					if(!file4.exists()){//読み込めなかった場合
-						throw new 	ArrayStoreException("予期せぬエラーが発生しました");
+						throw new 	IOException("予期せぬエラーが発生しました");
 					}else{//連番じゃなかった場合
-						throw new 	ArrayStoreException("売上ファイル名が連番になっていません");
+						throw new 	IllegalArgumentException("売上ファイル名が連番になっていません");
 					}
 				}		
 				FileReader fr4 = new FileReader(file4);
@@ -121,11 +127,13 @@ public class calculate_sales {
 					dataList.add(s4);
 					if(y == 0){ //１行目に支店コードがない場合
 						if(!branchmap1.containsKey(s4)){
-							throw new 	ArrayStoreException (er + ".rcdの支店コードが不正です");
+							br4.close();
+							throw new 	IllegalArgumentException (er + ".rcdの支店コードが不正です");
 						}
 					}else if(y == 1){//２行目に商品コードがない場合
 						if(!commap1.containsKey(s4)){
-							throw new 	ArrayStoreException(er + ".rcdの商品コードが不正です");	
+							br4.close();
+							throw new IllegalArgumentException(er + ".rcdの商品コードが不正です");	
 						}
 					}
 					y++;
@@ -133,15 +141,18 @@ public class calculate_sales {
 				//４行以上の場合
 				
 				if(y != 3){
-					throw new 	ArrayStoreException(er + ".rcdのフォーマットが不正です");	
+					br4.close();
+					throw new 	IllegalArgumentException(er + ".rcdのフォーマットが不正です");	
 				}
 				y = 0;
 				
 				int sum2 = Integer.parseInt(dataList.get(2));//今回取得した売上
 				sumdata1 = branchmap1.get(dataList.get(0)) + sum2;//支店売上
 				sumdata2 = commap1.get(dataList.get(1)) + sum2;//商品売上
-				if(sumdata1 >= Math.pow(10,10) || sumdata2 >= Math.pow(10,10)){
-					throw new ArrayStoreException("合計金額が10桁を超えました");
+				
+				if(sumdata1 >= 1000000000 || sumdata2 >= 1000000000){
+					br4.close();
+					throw new IllegalArgumentException("合計金額が10桁を超えました");
 				}
 				branchmap1.put(dataList.get(0),sumdata1);
 				branchmap3.put(dataList.get(0),sumdata1);//支店コードと売上
@@ -151,9 +162,8 @@ public class calculate_sales {
 				x++;
 				dataList.clear();
 				br4.close();
-			}catch(ArrayStoreException e){
+			}catch(IllegalArgumentException e){
 				System.out.println(e);
-				return;
 			}catch(IOException e){
 				System.out.println("予期せぬエラーが発生しました");
 			}
@@ -179,7 +189,12 @@ public class calculate_sales {
 				BranchList.put(branchmap3.get(s2),s2);//treemap にキー:売上金額 要素:支店コード
 				x++;
 			}
-			//出力
+			//書き込み
+			System.out.println("BranchList.firstKey　　："+ BranchList.firstKey());
+			System.out.println("BranchList.get　　:" + BranchList.get(0));
+			System.out.println("branchmap2.get　　；" + branchmap2.get(005));
+			System.out.println("branchmap2　　；" + branchmap2);
+			System.out.println(branchmap2.get(BranchList.get(BranchList.firstKey())));
 			bw5.write(BranchList.get(BranchList.firstKey()) + "," + branchmap2.get(BranchList.get(BranchList.firstKey())) + "," + BranchList.firstKey() + "\r\n");
 			s3 = BranchList.firstKey();
 			while(BranchList.higherKey(s3) != null){
@@ -208,14 +223,13 @@ public class calculate_sales {
 				ComList.put(commap3.get(s2),s2);//treemap にキー:売上金額 要素:商品コード
 				x++;
 			}
-			//出力
+			//書き込み
 			bw6.write(ComList.get(ComList.firstKey()) + "," + commap2.get(ComList.get(ComList.firstKey())) + "," + ComList.firstKey() + "\r\n");
 			s3 = ComList.firstKey();
 			while(ComList.higherKey(s3) != null){
 				bw6.write(ComList.get(ComList.higherKey(s3)) + "," + commap2.get(ComList.get(ComList.higherKey(s3))) + "," + ComList.higherKey(s3) + "\r\n");
 				s3 = ComList.higherKey(s3);
 			}
-			
 			bw6.close();
 		}catch(IOException e){
 			System.out.println("予期せぬエラーが発生しました");
